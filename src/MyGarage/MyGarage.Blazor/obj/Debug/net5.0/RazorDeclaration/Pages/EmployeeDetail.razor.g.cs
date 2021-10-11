a85eb444d6f4c5cc8e94e2c4f8d8a87675d8dffe
Kamil Szurgot <kamil.sz1999@gmail.com>
2021-10-11 13:16:37 +0200

    add EmployeeDetail page

--- src/MyGarage/MyGarage.Blazor/obj/Debug/net5.0/RazorDeclaration/Pages/EmployeeDetail.razor.g.cs
+++ src/MyGarage/MyGarage.Blazor/obj/Debug/net5.0/RazorDeclaration/Pages/EmployeeDetail.razor.g.cs
@@ -83,14 +83,14 @@ using MyGarage.Blazor.Shared;
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\kamil\Desktop\szkola\4 rok\7 semestr\inzynierka\MyGarage\src\MyGarage\MyGarage.Blazor\Pages\EmployeeOverview.razor"
+#line 2 "C:\Users\kamil\Desktop\szkola\4 rok\7 semestr\inzynierka\MyGarage\src\MyGarage\MyGarage.Blazor\Pages\EmployeeDetail.razor"
 using MyGarage.Shared;
 
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/employeeoverview")]
-    public partial class EmployeeOverview : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/employeedetail/{EmployeeId}")]
+    public partial class EmployeeDetail : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
@@ -98,28 +98,34 @@ using MyGarage.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 36 "C:\Users\kamil\Desktop\szkola\4 rok\7 semestr\inzynierka\MyGarage\src\MyGarage\MyGarage.Blazor\Pages\EmployeeOverview.razor"
-       
-    public IEnumerable<Employee> Employees { get; set; }
+#line 110 "C:\Users\kamil\Desktop\szkola\4 rok\7 semestr\inzynierka\MyGarage\src\MyGarage\MyGarage.Blazor\Pages\EmployeeDetail.razor"
+           
+        [Parameter]
+        public string EmployeeId { get; set; }
 
-    private List<Country> Countries { get; set; }
+        public Employee Employee { get; set; } = new Employee();
 
-    private List<JobCategory> JobCategories { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
 
-    protected override Task OnInitializedAsync()
-    {
+        private List<Country> Countries { get; set; }
 
-        InitializeCountries();
-        InitializeJobCategories();
-        InitializeEmployees();
+        private List<JobCategory> JobCategories { get; set; }
 
-        return base.OnInitializedAsync();
-    }
+        protected override Task OnInitializedAsync()
+        {
 
-    private void InitializeJobCategories()
-    {
-        JobCategories = new List<JobCategory>()
-    {
+            InitializeCountries();
+            InitializeJobCategories();
+            InitializeEmployees();
+            Employee = Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+
+            return base.OnInitializedAsync();
+        }
+
+        private void InitializeJobCategories()
+        {
+            JobCategories = new List<JobCategory>()
+{
         new JobCategory{JobCategoryId = 1, JobCategoryName = "Pie research"},
         new JobCategory{JobCategoryId = 2, JobCategoryName = "Sales"},
         new JobCategory{JobCategoryId = 3, JobCategoryName = "Management"},
@@ -131,12 +137,12 @@ using MyGarage.Shared;
         new JobCategory{JobCategoryId = 9, JobCategoryName = "Bakery"},
         new JobCategory{JobCategoryId = 9, JobCategoryName = "Bakery"}
     };
-    }
+        }
 
-    private void InitializeCountries()
-    {
-        Countries = new List<Country>
-    {
+        private void InitializeCountries()
+        {
+            Countries = new List<Country>
+{
         new Country {CountryId = 1, Name = "Belgium"},
         new Country {CountryId = 2, Name = "Netherlands"},
         new Country {CountryId = 3, Name = "USA"},
@@ -146,53 +152,54 @@ using MyGarage.Shared;
         new Country {CountryId = 7, Name = "France"},
         new Country {CountryId = 8, Name = "Brazil"}
     };
-    }
+        }
 
-    private void InitializeEmployees()
-    {
-        var e1 = new Employee
+        private void InitializeEmployees()
         {
-            CountryId = 1,
-            MaritalStatus = MaritalStatus.Single,
-            BirthDate = new DateTime(1989, 3, 11),
-            City = "Brussels",
-            Email = "bethany@bethanyspieshop.com",
-            EmployeeId = 1,
-            FirstName = "Bethany",
-            LastName = "Smith",
-            Gender = Gender.Female,
-            PhoneNumber = "324777888773",
-            Smoker = false,
-            Street = "Grote Markt 1",
-            Zip = "1000",
-            JobCategoryId = 1,
-            Comment = "Lorem Ipsum",
-            ExitDate = null,
-            JoinedDate = new DateTime(2015, 3, 1)
-        };
+            var e1 = new Employee
+            {
+                CountryId = 1,
+                MaritalStatus = MaritalStatus.Single,
+                BirthDate = new DateTime(1989, 3, 11),
+                City = "Brussels",
+                Email = "bethany@bethanyspieshop.com",
+                EmployeeId = 1,
+                FirstName = "Bethany",
+                LastName = "Smith",
+                Gender = Gender.Female,
+                PhoneNumber = "324777888773",
+                Smoker = false,
+                Street = "Grote Markt 1",
+                Zip = "1000",
+                JobCategoryId = 1,
+                Comment = "Lorem Ipsum",
+                ExitDate = null,
+                JoinedDate = new DateTime(2015, 3, 1)
+            };
 
-        var e2 = new Employee
-        {
-            CountryId = 2,
-            MaritalStatus = MaritalStatus.Married,
-            BirthDate = new DateTime(1979, 1, 16),
-            City = "Antwerp",
-            Email = "gill@bethanyspieshop.com",
-            EmployeeId = 2,
-            FirstName = "Gill",
-            LastName = "Cleeren",
-            Gender = Gender.Female,
-            PhoneNumber = "33999909923",
-            Smoker = false,
-            Street = "New Street",
-            Zip = "2000",
-            JobCategoryId = 1,
-            Comment = "Lorem Ipsum",
-            ExitDate = null,
-            JoinedDate = new DateTime(2017, 12, 24)
-        };
-        Employees = new List<Employee> { e1, e2 };
-    }
+            var e2 = new Employee
+            {
+                CountryId = 2,
+                MaritalStatus = MaritalStatus.Married,
+                BirthDate = new DateTime(1979, 1, 16),
+                City = "Antwerp",
+                Email = "gill@bethanyspieshop.com",
+                EmployeeId = 2,
+                FirstName = "Gill",
+                LastName = "Cleeren",
+                Gender = Gender.Female,
+                PhoneNumber = "33999909923",
+                Smoker = false,
+                Street = "New Street",
+                Zip = "2000",
+                JobCategoryId = 1,
+                Comment = "Lorem Ipsum",
+                ExitDate = null,
+                JoinedDate = new DateTime(2017, 12, 24)
+            };
+            Employees = new List<Employee> { e1, e2 };
+        }
+    
 
 #line default
 #line hidden

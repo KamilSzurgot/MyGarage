684ef672e98bb9123d4bc4c42ac259bf520685ef
Kamil Szurgot <kamil.sz1999@gmail.com>
2021-10-11 19:16:09 +0200

    add new app.css file, edit index.razor/html, add photos

--- src/MyGarage/MyGarage.Blazor/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ src/MyGarage/MyGarage.Blazor/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -89,21 +89,6 @@ using MyGarage.Blazor.Shared;
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 28 "C:\Users\kamil\Desktop\szkola\4 rok\7 semestr\inzynierka\MyGarage\src\MyGarage\MyGarage.Blazor\Shared\NavMenu.razor"
-       
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591

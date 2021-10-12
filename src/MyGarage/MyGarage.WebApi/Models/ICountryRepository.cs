974a885e584daf51459214aaf469f727453ec083
Kamil Szurgot <kamil.sz1999@gmail.com>
2021-10-12 18:04:25 +0200

    add controllers, models

---
+++ src/MyGarage/MyGarage.WebApi/Models/ICountryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MyGarage.Shared;
+
+namespace MyGarage.WebApi.Models
+{
+    public interface ICountryRepository
+    {
+        IEnumerable<Country> GetAllCountries();
+        Country GetCountryById(int countryId);
+    }
+}
---
+++ src/MyGarage/MyGarage.WebApi/Models/IJobCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MyGarage.Shared;
+
+namespace MyGarage.WebApi.Models
+{
+    public interface IJobCategoryRepository
+    {
+        IEnumerable<JobCategory> GetAllJobCategories();
+        JobCategory GetJobCategoryById(int jobCategoryId);
+    }
+}

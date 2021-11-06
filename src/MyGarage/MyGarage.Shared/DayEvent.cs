11fa8c81deff51e4c638eeea57438969a95e2db5
Kamil Szurgot <kamil.sz1999@gmail.com>
2021-11-06 22:11:32 +0100

    add calendar head and body, add note modal

--- src/MyGarage/MyGarage.Shared/DayEvent.cs
+++ src/MyGarage/MyGarage.Shared/DayEvent.cs
@@ -14,6 +14,7 @@ namespace MyGarage.Shared
         public DateTime FromDate { get; set; } = new DateTime(1900, 1, 1);
         public DateTime ToDate { get; set; } = new DateTime(1900, 1, 1);
         public string DateValue { get; set; }
+        public string DayName { get; set; }
         public string Message { get; set; }
     }
 }

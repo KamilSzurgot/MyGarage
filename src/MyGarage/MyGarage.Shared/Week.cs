8d0877da10c30c893a76773d2cff96172b1f73ca
Kamil Szurgot <kamil.sz1999@gmail.com>
2021-11-06 21:05:03 +0100

    add DayEvent and Week class, add option to change month in Calendar.razor

---
+++ src/MyGarage/MyGarage.Shared/Week.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGarage.Shared
+{
+    public class Week
+    {
+        public List<DayEvent> Dates { get; set; } = new List<DayEvent>();
+    }
+}
